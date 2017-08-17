56dda0d28ba4f4c5e29d06c0fffa2d540417206a
wdk956093628 <www.956093628@qq.com>
2017-08-17 13:33:14 +0800

    version 0.0.0

---
+++ js/index_js.js
@@ -0,0 +1,72 @@
+window.onload=function(){
+    //获取dom节点
+    var timer=null;
+    var timer1=null;
+    var img_list=getObj("img_list");
+    var prev=getObj("prev");
+    var next=getObj("next");
+    var buttons=document.getElementsByTagName("span");
+    //绑定事件
+    prev.addEventListener("click",function(){
+        scroll("-3038px",0,1519)
+    });
+    next.addEventListener("click",function(){
+        scroll(0,-3038,-1519)
+    });
+
+    for(var i=0;i<buttons.length;i++){
+        (function(n){
+            buttons[n].onclick=function(){
+                button_control(this,n);
+            }
+        })(i)
+    }
+
+    //事件处理函数
+    //左右箭头控制
+    function scroll(origin,ter,speed){
+        var left_val=getStyle(img_list,"left");
+        if(left_val===ter){
+            img_list.style.left=origin;
+            left_val=parseInt(img_list.style.left);
+        }else {
+            left_val+=speed;
+            img_list.style.left=left_val+"px";
+        }
+
+        //在修改class前，清空所有class；
+        for(var i=0;i<buttons.length;i++){
+            buttons[i].setAttribute("class","");
+        }
+        //点击span修改class为“on”；
+        buttons[left_val/-1519].setAttribute("class","sty");
+    }
+    //底部按钮控制
+    function button_control(that,num) {
+        //在修改class前，清空所有class；
+        for(var i=0;i<buttons.length;i++){
+            buttons[i].setAttribute("class","");
+        }
+        //点击span修改class为“on”；
+        that.setAttribute("class","sty");
+        //控制图片移动到当前位置
+        img_list.style.left=num*(-1519)+"px";
+    }
+    //添加定时器
+    timer=setInterval(function(){
+        scroll(0,-3038,-1519);
+    },2000);
+
+    getObj("scroll").onmouseover=function(){
+        clearInterval(timer);
+        clearInterval(timer1)
+    };
+    getObj("scroll").onmouseout=function(){
+        clearInterval(timer);
+        clearInterval(timer1);
+        timer1=setInterval(function(){
+            scroll(0,-3038,-1519)
+        },2000);
+    }
+
+};
---
+++ js/tools.js
@@ -0,0 +1,14 @@
+function getObj(id){
+    return document.getElementById(id);
+}
+//获取样式表样式
+function getStyle(obj,attr){
+//        document.all为判断IE浏览器的条件
+    if(document.all){
+//            兼容IE8及以下版本
+        return parseInt(obj.currentStyle[attr]);
+    }else{
+//            兼容火狐和谷歌
+        return parseInt(getComputedStyle(obj,false)[attr]);
+    }
+}
